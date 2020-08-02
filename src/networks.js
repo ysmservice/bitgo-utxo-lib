60f4ae3056cbd8c295571c9c6480058ae9a653b1
Asher Dawes <asher.dawes@gmail.com>
2020-08-01 17:21:04 -0700

    Update Zcash consensus branch ID

--- src/networks.js
+++ src/networks.js
@@ -110,7 +110,7 @@ module.exports = {
       1: 0x00,
       2: 0x00,
       3: 0x5ba81b19,
-      4: 0x2bb40e60
+      4: 0xf5b9230b
     },
     coin: coins.ZEC,
     isZcash: true
@@ -129,7 +129,7 @@ module.exports = {
       1: 0x00,
       2: 0x00,
       3: 0x5ba81b19,
-      4: 0x2bb40e60
+      4: 0xf5b9230b
     },
     coin: coins.ZEC,
     isZcash: true
